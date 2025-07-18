1051793f4155e9d648bf09dc41673b948aa41670
daourO1 <daoursambandaw@gmail.com>
2025-07-18 20:43:17 +0000

    correction

--- codes/Donnee_Nettoyer.py
+++ codes/Donnee_Nettoyer.py
@@ -14,6 +14,7 @@ options = Options()
 options.add_argument("--headless=new") 
 options.add_argument("--disable-gpu")
 options.add_argument("--no-sandbox")
+options.add_argument('--disable-dev-shm-usage')
 service = Service(ChromeDriverManager().install())
 driver = webdriver.Chrome(service=service, options=options)
 
